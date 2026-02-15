37a2998a391d4e45394f441a3d0dc6db38ded200
vikramd7 <vikramd11275@gmail.com>
2026-02-15 00:13:59 -0700

    did some changes for accuracy

--- Logic/features.py
+++ Logic/features.py
@@ -11,15 +11,15 @@ def normalize_text(text: str) -> str:
 
 def add_manual_features(texts):
     features = []
-
     for text in texts:
+        t = text.lower()
         features.append([
-            int("you" in text),
-            int("always" in text or "never" in text),
-            int("everyone" in text or "nobody" in text),
-            int("if" in text and "then" in text),
-            int("because" in text),
-            len(text.split())
+            int("either" in t and "or" in t),
+            int("if" in t and ("then" in t or "eventually" in t or "soon" in t)),
+            int("so you" in t or "so you're" in t),
+            int("you clearly" in t or "you obviously" in t),
+            int("only a" in t or "typical" in t),
+            len(t.split())
         ])
+    return np.array(features, dtype=np.float64)
 
-    return np.array(features)
